aa8a534bb192fba7e69d0a316899eb197efdf424
Usuario <Usuario@DESKTOP-6026QTA>
2024-12-16 17:06:51 -0500

    Agregar archivos de proyecto.

---
+++ FashionLine/Services/MongoDbService.cs
@@ -0,0 +1,20 @@
+﻿using MongoDB.Driver;
+
+namespace FashionLine.Services
+{
+    public class MongoDbService
+    {
+        private readonly IMongoDatabase _database;
+
+        public MongoDbService(IConfiguration configuration)
+        {
+            var client = new MongoClient(configuration["MongoDB:ConnectionString"]);
+            _database = client.GetDatabase(configuration["MongoDB:DatabaseName"]);
+        }
+
+        public IMongoCollection<T> GetCollection<T>(string name)
+        {
+            return _database.GetCollection<T>(name);
+        }
+    }
+}
---
+++ FashionLine/Services/ProductService.cs
@@ -0,0 +1,30 @@
+﻿using FashionLine.Models;
+using MongoDB.Driver;
+
+namespace FashionLine.Services
+{
+    public class ProductService
+    {
+        private readonly IMongoCollection<Producto> _products;
+
+        public ProductService(MongoDbService mongoDbService)
+        {
+            _products = mongoDbService.GetCollection<Producto>("Productos");
+        }
+
+        public async Task<List<Producto>> GetAllAsync() =>
+            await _products.Find(_ => true).ToListAsync();
+
+        public async Task<Producto> GetByIdAsync(string id) =>
+            await _products.Find(p => p.Id == id).FirstOrDefaultAsync();
+
+        public async Task CreateAsync(Producto product) =>
+            await _products.InsertOneAsync(product);
+
+        public async Task UpdateAsync(string id, Producto updatedProduct) =>
+            await _products.ReplaceOneAsync(p => p.Id == id, updatedProduct);
+
+        public async Task DeleteAsync(string id) =>
+            await _products.DeleteOneAsync(p => p.Id == id);
+    }
+}
